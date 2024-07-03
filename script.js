910e08866e57c28237cb28eb8a7f27b9947376dc
MhayreeJhaine <114218272+MhayreeJhaine@users.noreply.github.com>
2024-07-03 23:58:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const date = new Date();
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const getTime = () => {
+  let hours = date.getUTCHours();
+  let minutes = date.getUTCMinutes();
+  const ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  const strTime = hours + ":" + minutes + ampm + " UTC";
+
+  document.getElementById("currentTime").innerHTML = strTime;
+};
+
+const getDay = () => {
+  const day = days[date.getDay()];
+
+  document.getElementById("currentDay").innerHTML = day;
+};
+
+getTime();
+getDay();
